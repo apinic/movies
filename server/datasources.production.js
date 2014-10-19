d8113169c1aff3de9d16f0515150798f222e34d5
Paulo McNally <paulomcnally@gmail.com>
2014-10-18 19:52:07 -0600

    fix connection

---
+++ server/datasources.production.js
@@ -0,0 +1,11 @@
+{
+  db: {
+    name: 'db'
+    connector: 'postgresql'
+    host: process.env.POSTGRESQL_HOST
+    username: process.env.POSTGRESQL_USERNAME
+    password: process.env.POSTGRESQL_PASSWORD
+    database: process.env.POSTGRESQL_DATABASE
+    ssl: true
+  }
+}
