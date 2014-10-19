8ba91cdd1a0c08266a35e4e8f57e628ceb9d7520
Paulo McNally <paulomcnally@gmail.com>
2014-10-18 18:02:16 -0600

    test

--- controllers/1.js
+++ controllers/1.js
@@ -22,6 +22,7 @@ module.exports.add = function(req, res) {
   var genres = req.body.genres;
   var accessToken = req.body['access_token'];
   var theaters = req.body.theaters;
+  res.send(theaters);
 
   if (process.env.APINIC_MOVIES_ACCESS_TOKEN !== accessToken) {
     res.json({error:{message:'access_token inválido.'}});
