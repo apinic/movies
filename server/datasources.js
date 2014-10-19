2b1599fc0093973983098ef472c5272e0fa66bf6
Paulo McNally <paulomcnally@gmail.com>
2014-10-18 20:10:50 -0600

    fix datasource

--- server/datasources.js
+++ server/datasources.js
@@ -1,11 +1,11 @@
-{
+module.exports = {
   db: {
-    name: 'db'
-    connector: 'postgresql'
-    host: process.env.POSTGRESQL_HOST
-    username: process.env.POSTGRESQL_USERNAME
-    password: process.env.POSTGRESQL_PASSWORD
-    database: process.env.POSTGRESQL_DATABASE
+    name: 'db',
+    connector: 'postgresql',
+    host: process.env.POSTGRESQL_HOST,
+    username: process.env.POSTGRESQL_USERNAME,
+    password: process.env.POSTGRESQL_PASSWORD,
+    database: process.env.POSTGRESQL_DATABASE,
     ssl: true
   }
-}
+};
